919743d6161b1c43fa761947510e91e104c5f862
xiachang <14699454@qq.com>
2018-08-27 23:41:43 +0800

    BinarySearchTree Delete

--- src/Table/BinarySearchTree.go
+++ src/Table/BinarySearchTree.go
@@ -16,6 +16,10 @@ type BinarySearchTree struct{
 	Root *Node
 }
 
+func NewBinarySearchTree() *BinarySearchTree{
+	return &BinarySearchTree{}
+}
+
 func(this *BinarySearchTree) Get(key int) string{
 	return this.RetrieveFromRoot(this.Root,key)
 }
@@ -33,7 +37,7 @@ func(this *BinarySearchTree) CompareKey(key1 int,key2 int) int{
 func(this *BinarySearchTree) RetrieveFromRoot(n *Node,key int) string{
 
 	if n==nil{
-		return ""
+		return "Not exist in tree."
 	}
 
 	compareResult:=this.CompareKey(key,n.Key)
@@ -48,8 +52,33 @@ func(this *BinarySearchTree) RetrieveFromRoot(n *Node,key int) string{
 
 }
 
+func(this *BinarySearchTree) CalcucateSize(n *Node){
+	size:=0
+
+	if(n.Left!=nil){
+		size+=n.Left.Size
+	}
+
+	if(n.Right!=nil){
+		size+=n.Right.Size
+	}
+
+	if size==0{
+		n.Size=1
+	}else{
+		n.Size=size+1
+	}
+}
+
 func(this *BinarySearchTree) Put(key int,val string){
-	this.InsertNode(this.Root,key,val)
+
+	if(this.Root==nil){
+		this.Root=NewNode(key,val,1)
+		return
+	}
+
+	this.Root=this.InsertNode(this.Root,key,val)
+
 }
 
 func(this *BinarySearchTree) InsertNode(n *Node,key int,val string) *Node{
@@ -70,8 +99,90 @@ func(this *BinarySearchTree) InsertNode(n *Node,key int,val string) *Node{
 		n.Val=val
 	}
 
-	n.Size=n.Left.Size+n.Right.Size
+	this.CalcucateSize(n)
 
 	//This node is not new node that inserted.It's the parent node of inserted node.
 	return n
 }
+
+func(this *BinarySearchTree) DeleteMin(){
+	this.DeleteMinByNode(this.Root)
+}
+
+func(this *BinarySearchTree) DeleteMinByNode(n *Node) *Node {
+
+	//最小节点的左子节点一定为nil，直接返回右节点，如果是右子节点是nil则不管；如果不为空，它也一定小于要删除节点的父节点，所以
+	//直接嫁接在父节点的左子节点上即可
+	if(n.Left==nil){
+		return n.Right
+	}
+
+	//这里n是要删除节点的父节点，因为要删除的节点小于它，所以直接把要删除节点的右子节点嫁接在该父节点的左子节点下即可
+	//此处是递归，所以最后一定会递归到第一个传入的节点
+	n.Left=this.DeleteMinByNode(n.Left)
+
+	this.CalcucateSize(n)
+
+	return n
+}
+
+func(this *BinarySearchTree) Delete(key int){
+	this.Root=this.DeleteByKey(this.Root,key)
+}
+
+func(this *BinarySearchTree) Min(n *Node) *Node{
+	if n.Left==nil{
+		return n
+	}
+
+	return this.Min(n.Left)
+}
+
+
+func(this *BinarySearchTree) DeleteByKey(n *Node,key int) *Node{
+	if n==nil{
+		return nil
+	}
+
+	compareResult:=this.CompareKey(key,n.Key)
+
+	if compareResult<0{
+		n.Left=this.DeleteByKey(n.Left,key)
+	}else if compareResult>0{
+		n.Right=this.DeleteByKey(n.Right,key)
+	}else{
+		//找到了对应key的Node
+		//3种情况
+		//1 右节点为空，直接返回左节点，嫁接在父节点的左节点即可
+		//2 左节点为空，直接返回右节点，嫁接在父节点的右节点即可
+		//3 左右节点都不为空，找到右节点下面的最小节点，
+
+		if n.Right==nil{
+			return n.Left
+		}
+
+		if n.Left==nil{
+			return n.Right
+		}
+
+		tmpNode:=n
+		//找到要删除节点的所有子节点中最小的节点
+		n=this.Min(tmpNode.Right)
+
+		//将要删除节点的右子节点下最小节点删除，并整理完毕后，返回右子节点，实际上删除的就是n这个节点
+		//因为是最小节点，所以直接可以嫁接至右子节点
+		n.Right=this.DeleteMinByNode(tmpNode.Right)
+
+		//因为n是从tmpNode的右子节点中查找出的最小节点，但一定大于tmpNode，所以直接将tmpNode的左子节点嫁接给它即可
+		n.Left=tmpNode.Left
+
+	}
+
+	this.CalcucateSize(n)
+
+	return n
+}
+
+
+
+
--- src/Table/table_test.go
+++ src/Table/table_test.go
@@ -22,3 +22,52 @@ func TestBinarySearchTable(t *testing.T) {
 
 	fmt.Println(val2)
 }
+
+func TestBinarySearchTree(t *testing.T) {
+
+	/*
+	如果输入的数是顺序的，则二叉会退化成无序链表
+	12,11,10,9 会让树变成只有右子树，不会形成lgn的树高度
+	12
+		11
+			10
+				9
+
+	 */
+
+	bsTree:=NewBinarySearchTree()
+
+	bsTree.Put(12,"12")
+	bsTree.Put(3,"3")
+	bsTree.Put(2,"2")
+	bsTree.Put(5,"5")
+	bsTree.Put(4,"4")
+
+	bsTree.Put(11,"11")
+	bsTree.Put(13,"13")
+
+	bsTree.Put(15,"15")
+
+	fmt.Println(bsTree.Root.Val)
+	fmt.Println(bsTree.Get(5))
+	fmt.Println(bsTree.Get(11))
+	fmt.Println(bsTree.Root.Left.Right.Left.Val)
+
+	//bsTree.DeleteMin()
+
+	key3Val:=bsTree.Get(3)
+	fmt.Println(key3Val)
+
+	/*
+	The tree is:
+				12
+		3				13
+	2		5
+		4		11
+
+	delete 3 , node 4 will be set the positoin of node 3.
+	 */
+
+	bsTree.Delete(3)
+	fmt.Println(bsTree.Root.Left.Val)
+}
